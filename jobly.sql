1d3a316d3de91d4455145ca31e736c7795c9094a
Diana Huang <dianahuang@Dianas-MBP.lan>
2022-08-01 13:33:09 -1000

    commit

--- jobly.sql
+++ jobly.sql
@@ -1,18 +1,9 @@
-\echo 'Delete and recreate jobly db?'
-\prompt 'Return for yes or control-C to cancel > ' foo
+\ echo 'Delete and recreate jobly db?' \ prompt 'Return for yes or control-C to cancel > ' foo DROP DATABASE IF EXISTS jobly;
 
-DROP DATABASE jobly;
 CREATE DATABASE jobly;
-\connect jobly
 
-\i jobly-schema.sql
-\i jobly-seed.sql
+\ connect jobly \ i jobly - schema.sql \ i jobly - seed.sql \ echo 'Delete and recreate jobly_test db?' \ prompt 'Return for yes or control-C to cancel > ' foo DROP DATABASE IF EXISTS jobly_test;
 
-\echo 'Delete and recreate jobly_test db?'
-\prompt 'Return for yes or control-C to cancel > ' foo
-
-DROP DATABASE jobly_test;
 CREATE DATABASE jobly_test;
-\connect jobly_test
 
-\i jobly-schema.sql
+\ connect jobly_test \ i jobly - schema.sql
--- routes/companies.js
+++ routes/companies.js
@@ -14,7 +14,6 @@ const companyUpdateSchema = require("../schemas/companyUpdate.json");
 
 const router = new express.Router();
 
-
 /** POST / { company } =>  { company }
  *
  * company should be { handle, name, description, numEmployees, logoUrl }
@@ -28,7 +27,7 @@ router.post("/", ensureLoggedIn, async function (req, res, next) {
   try {
     const validator = jsonschema.validate(req.body, companyNewSchema);
     if (!validator.valid) {
-      const errs = validator.errors.map(e => e.stack);
+      const errs = validator.errors.map((e) => e.stack);
       throw new BadRequestError(errs);
     }
 
@@ -51,8 +50,19 @@ router.post("/", ensureLoggedIn, async function (req, res, next) {
  */
 
 router.get("/", async function (req, res, next) {
+  const q = req.query;
+  // arrive as strings from querystring, but we want as ints
+  if (q.minEmployees !== undefined) q.minEmployees = +q.minEmployees;
+  if (q.maxEmployees !== undefined) q.maxEmployees = +q.maxEmployees;
+
   try {
-    const companies = await Company.findAll();
+    const validator = jsonschema.validate(q, companySearchSchema);
+    if (!validator.valid) {
+      const errs = validator.errors.map((e) => e.stack);
+      throw new BadRequestError(errs);
+    }
+
+    const companies = await Company.findAll(q);
     return res.json({ companies });
   } catch (err) {
     return next(err);
@@ -91,7 +101,7 @@ router.patch("/:handle", ensureLoggedIn, async function (req, res, next) {
   try {
     const validator = jsonschema.validate(req.body, companyUpdateSchema);
     if (!validator.valid) {
-      const errs = validator.errors.map(e => e.stack);
+      const errs = validator.errors.map((e) => e.stack);
       throw new BadRequestError(errs);
     }
 
@@ -115,6 +125,5 @@ router.delete("/:handle", ensureLoggedIn, async function (req, res, next) {
     return next(err);
   }
 });
-
 
 module.exports = router;
